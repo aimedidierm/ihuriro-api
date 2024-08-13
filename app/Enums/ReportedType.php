855d58ae26bc42f375d34cd72e4b6a0e87d89d48
Aime Didier <aimedidiermugisha@gmail.com>
2024-08-13 07:37:40 +0200

    Refactor reported types by removing spaces

--- app/Enums/ReportedType.php
+++ app/Enums/ReportedType.php
@@ -5,9 +5,9 @@ namespace App\Enums;
 enum ReportedType: string
 {
     case CRIME = 'Crime';
-    case PROPERTYDAMAGE = 'Property damage';
-    case ACCIDENTREPORT = 'Accident report';
-    case FIREINCIDENT = 'Fire incident';
-    case WEATHERINCIDENT = 'Weather incident';
+    case PROPERTYDAMAGE = 'PropertyDamage';
+    case ACCIDENTREPORT = 'AccidentReport';
+    case FIREINCIDENT = 'FireIncident';
+    case WEATHERINCIDENT = 'WeatherIncident';
     case OTHERS = 'Others';
 }
